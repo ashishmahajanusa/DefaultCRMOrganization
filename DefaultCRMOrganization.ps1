7eed40f6718de07153fd7bb8fba14e3bbf5a855e
ashishmahajan_cp <SND\ashishmahajan_cp>
2012-06-12 05:00:25 +0000

    First version 1.0 of my project "This PowerShell script will output details of your default organization".

---
+++ DefaultCRMOrganization.ps1
@@ -0,0 +1,9 @@
+# ----------------------------------------
+# Developer: Ashish Mahajan
+# Email: ashishmahajan@hotmail.com
+# Website: ashishmahajan.com
+# Script Definition: This script will output details of your default organization.
+# ----------------------------------------
+add-pssnapin Microsoft.CRM.Powershell
+get-crmorganization
+read-host 'Press press enter key to close this window. . .'
